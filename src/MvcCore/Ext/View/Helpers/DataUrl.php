7f53df6a37680b299b7dee7d0cc48018fbe46cc6
Tom Flídr <tomflidr@gmail.com>
2018-05-04 16:28:42 +0200

    release 5 alpha

---
+++ src/MvcCore/Ext/View/Helpers/DataUrl.php
@@ -0,0 +1,104 @@
+<?php
+
+/**
+ * MvcCore
+ *
+ * This source file is subject to the BSD 3 License
+ * For the full copyright and license information, please view
+ * the LICENSE.md file that are distributed with this source code.
+ *
+ * @copyright	Copyright (c) 2016 Tom Flídr (https://github.com/mvccore/mvccore)
+ * @license		https://mvccore.github.io/docs/mvccore/5.0.0/LICENCE.md
+ */
+
+namespace MvcCore\Ext\View\Helpers;
+
+/**
+ * Responsibility - get any file content by given relative or absolute path in data url format: `data:image/png;base64,iVBOR..`.
+ * - Path could be relative from currently rendered view,
+ *   relative from application root or absolute path to file.
+ * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/Data_URIs
+ */
+class DataUrl implements \MvcCore\Ext\View\Helpers\IHelper
+{
+	/**
+	 * MvcCore Extension - View Helper - Assets - version:
+	 * Comparation by PHP function version_compare();
+	 * @see http://php.net/manual/en/function.version-compare.php
+	 */
+	const VERSION = '5.0.0-alpha';
+
+	/**
+	 * Currently rendered view instance.
+	 * @var \MvcCore\View|\MvcCore\Interfaces\IView|NULL
+	 */
+	protected $view = NULL;
+
+	/**
+	 * Application root path on hard drive.
+	 * Example: `"C:/www/my/development/direcotry/www"`
+	 * @var string|NULL
+	 */
+	protected $appRoot			= NULL;
+
+	/**
+	 * Create view helper instance, everytime new instance.
+	 * @return \MvcCore\Ext\View\Helpers\DataUrl
+	 */
+	public static function & GetInstance () {
+		$instance = new static;
+		return $instance;
+	}
+
+	/**
+	 * Set currently rendered view instance every time this helper
+	 * is called and the rendered view instance is changed.
+	 * This method sets these protected object references:
+	 * - `DataUrl::$view`		as `\MvcCore\View|\MvcCore\Interfaces\IView`
+	 * - `DataUrl::$request`	as `\MvcCore\Request|\MvcCore\Interfaces\IRequest`
+	 * @param \MvcCore\View|\MvcCore\Interfaces\IView $view
+	 * @return \MvcCore\Ext\View\Helpers\DataUrl
+	 */
+	public function & SetView (\MvcCore\Interfaces\IView & $view) {
+		$this->view = $view;
+		$this->appRoot = $view->GetController()->GetRequest()->GetAppRoot();
+		return $this;
+	}
+
+	/**
+	 * Return any file content by given relative or absolute path in data url.
+	 * Path could be relative from currently rendered view,
+	 * relative from application root or absolute path to file.
+	 * @see https://developer.mozilla.org/en-US/docs/Web/HTTP/Basics_of_HTTP/Data_URIs
+	 * @param string $relativeOrAbsolutePath
+	 * @throws \InvalidArgumentException If file not found by given path in any searched place.
+	 * @return string Data url value like: `data:image/png;base64,iVBORw0KGgoAAAANSUhEUgA...`
+	 */
+	public function DataUrl ($relativeOrAbsolutePath) {
+		$currentDirFullPath = $this->view->GetCurrentViewDirectory();
+		$searchedPaths = array();
+		// first - try to find file relatively from currently rendered view
+		array_unshift($searchedPaths, $currentDirFullPath . '/' . $relativeOrAbsolutePath);
+		if (!file_exists($searchedPaths[0])) {
+			// second - try to find file relatively from application root
+			array_unshift($searchedPaths, $this->appRoot . '/' . ltrim($relativeOrAbsolutePath, '/'));
+			if (!file_exists($searchedPaths[0])) {
+				// third - try to find file absolutely
+				array_unshift($searchedPaths, $relativeOrAbsolutePath);
+				if (!file_exists($searchedPaths[0])) {
+					// throw an error at last
+					throw new \InvalidArgumentException(
+						"[".__CLASS__."] File not found in paths: '" . implode("', '", array_reverse($searchedPaths)) . "'."
+					);
+				}
+			}
+		}
+		// get last searched path
+		$fileFullPath = $searchedPaths[0];
+		// Read image path, convert to base64 encoding
+		$imageData = base64_encode(file_get_contents($fileFullPath));
+		// Format the image SRC:  data:{mime};base64,{data};
+		$result = 'data: '.mime_content_type($fileFullPath).';base64,'.$imageData;
+		return $result;
+	}
+}
