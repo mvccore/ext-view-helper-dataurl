ad079b794d041ab1389f357f8bfbd091ac204ef4
Tom Flídr <tomflidr@gmail.com>
2024-11-21 13:45:37 +0100

    release v5.3

--- src/MvcCore/Ext/Views/Helpers/DataUrlHelper.php
+++ src/MvcCore/Ext/Views/Helpers/DataUrlHelper.php
@@ -27,7 +27,7 @@ class DataUrlHelper implements \MvcCore\Ext\Views\Helpers\IHelper {
 	 * Comparison by PHP function version_compare();
 	 * @see http://php.net/manual/en/function.version-compare.php
 	 */
-	const VERSION = '5.2.0';
+	const VERSION = '5.3.0';
 
 	/**
 	 * Currently rendered view instance.
